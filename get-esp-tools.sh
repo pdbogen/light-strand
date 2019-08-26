604e48bac4af8d204c1a0910201e884e38073c9e
Patrick Bogen <pdbogen@cernu.us>
2019-08-25 20:28:39 -0700

    fix path

--- get-esp-tools.sh
+++ get-esp-tools.sh
@@ -11,4 +11,4 @@ curl -s -f https://api.github.com/repos/espressif/arduino-esp32/releases > "$TMP
 tag="$(jq -r 'map(select(.prerelease|not))[0].tag_name' < "$TMP/releases.json")"
 curl -L -s -f "https://github.com/espressif/arduino-esp32/releases/download/${tag}/esp32-${tag}.zip" > "$TMP/release.zip"
 unzip "$TMP/release.zip"
-mv "esp-${tag}" "esp32"
+mv "esp32-${tag}" "esp32"
